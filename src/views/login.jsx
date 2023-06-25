8b73114f5ddf45661f8c9e1e67e5c852a79926bc
JoséZevs <100696361+Jose-Vitor-Sousa-Cardoso-Requena@users.noreply.github.com>
2023-06-25 16:27:37 -0300

    Initial commit

---
+++ src/views/login.jsx
@@ -0,0 +1,10 @@
+//import Button  from '../components/button/button'; 
+
+export default function Login() {
+    return(
+        <> 
+            <img src="../src/assets/BgMobile.png">
+            </img>
+        </>
+    )
+}

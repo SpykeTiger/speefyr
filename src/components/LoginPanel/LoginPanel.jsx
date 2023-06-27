7550174b633c95e59f6b6d5f99f47168a7e6a367
JoséZevs <100696361+Jose-Vitor-Sousa-Cardoso-Requena@users.noreply.github.com>
2023-06-27 00:08:58 -0300

    mudanças 4

--- src/components/LoginPanel/LoginPanel.jsx
+++ src/components/LoginPanel/LoginPanel.jsx
@@ -1,12 +1,15 @@
 import styles from './LoginPanel.scss';
 export function LoginPanel() {
     return(
-        <div className='container'>
-            <h2>Speefyr</h2>
-            <input type="text" placeholder="Usuário"></input>
-            <input type="password" placeholder="Senha"></input>
-            <button type="submit">Entrar</button>
-        </div>  
+        <div className='loginpanel'>
+            <div className='container'>
+                <h2>Speefyr</h2>
+                <input type="text" placeholder="Usuário"></input>
+                <input type="password" placeholder="Senha"></input>
+                <button type="submit">Entrar</button>
+            </div>  
+        </div>
+        
        
     )
 }

a41b9e3e8b5bc6d350d0729e2bea71befed9c4b0
JoséZevs <100696361+Jose-Vitor-Sousa-Cardoso-Requena@users.noreply.github.com>
2023-06-28 11:45:21 -0300

    Mudanças no Home

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,52 +1,44 @@
 import styles from './SideBar.scss' 
 export default function SideBar(){
     return(
+        <>
         <div class="container_SideBar">
             <nav>
                 <div class="navbar">
                     <div class="logo">
-                    <img src="/pic/logo.jpg" alt=""></img>
-                    <h1>jobs</h1>
+                        <img src='../assets/Logo.Png' alt=""></img>
+                        <h1>Speefyr</h1>
                     </div>
                     <ul>
-                    <li><a href="#">
-                        <i class="fas fa-user"></i>
-                        <span class="nav-item">Dashboard</span>
-                    </a>
-                    </li>
-                    <li><a href="#">
-                        <i class="fas fa-chart-bar"></i>
-                        <span class="nav-item">Analytics</span>
-                    </a>
-                    </li>
-                    <li><a href="#">
-                        <i class="fas fa-tasks"></i>
-                        <span class="nav-item">Jobs Board</span>
-                    </a>
-                    </li>
-                    <li><a href="#">
-                        <i class="fab fa-dochub"></i>
-                        <span class="nav-item">Documnents</span>
-                    </a>
-                    </li>
-                    <li><a href="#">
-                        <i class="fas fa-cog"></i>
-                        <span class="nav-item">Setting</span>
-                    </a>
-                    </li>
-                    <li><a href="#">
-                        <i class="fas fa-question-circle"></i>
-                        <span class="nav-item">Help</span>
-                    </a>
-                    </li>
-                    <li><a href="#" class="logout">
-                        <i class="fas fa-sign-out-alt"></i>
-                        <span class="nav-item">Logout</span>
-                    </a>
-                    </li>
+                        <li><a href="#">
+                            <i class="fas fa-user"></i>
+                            <span class="nav-item">Conta</span>
+                        </a>
+                        </li>
+                        <li><a href="#">
+                            <i class="fas fa-chart-bar"></i>
+                            <span class="nav-item">Estatística</span>
+                        </a>
+                        </li>
+                        <li><a href="#">
+                            <i class="fas fa-cog"></i>
+                            <span class="nav-item">Configurações</span>
+                        </a>
+                        </li>
+                        <li><a href="#">
+                            <i class="fas fa-question-circle"></i>
+                            <span class="nav-item">Ajuda</span>
+                        </a>
+                        </li>
+                        <li><a href="#" class="logout">
+                            <i class="fas fa-sign-out-alt"></i>
+                            <span class="nav-item">Logout</span>
+                        </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
         </div>
+        </>
     )
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,13 @@
 import SideBar from '../../components/SideBar/SideBar.jsx';
+
+import styles from './Home.scss';
 export default function Home() {
     return(
         <>
-            <div className='Home'>
-                <SideBar/>
+            <div className='PagHome'>
+                <div className='Home'>
+                    <SideBar/>
+                </div>
             </div>
         </>
     )

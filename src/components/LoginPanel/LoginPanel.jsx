07fcef50934936056a0d912a4adda8396350b7ed
vinic <viniciusgroto@gmail.com>
2023-06-28 11:43:44 -0300

    redirect

--- src/components/LoginPanel/LoginPanel.jsx
+++ src/components/LoginPanel/LoginPanel.jsx
@@ -1,15 +1,23 @@
 import styles from './LoginPanel.scss';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 export function LoginPanel() {
+    
+    const navigate = useNavigate();
+
+    const redirecionarParaOutraRota = () => {
+        navigate("/Home")
+    };
     return(
+        
         <div id='loginpanel'>
             <div className='loginpanel'>
                 <div className='container'>
                     <h2>Speefyr</h2>
                     <input type="text" placeholder="Usuário"></input>
                     <input type="password" placeholder="Senha"></input>
-                    <button type="submit" onClick={<Link to='/Home'></Link>}>Entrar</button>
+                    <button type="submit" onClick={redirecionarParaOutraRota} >Entrar</button>
                 </div>  
             </div>
         </div>
